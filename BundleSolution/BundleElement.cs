a9a68b24de6a19ff3143b267ff14aebb05625126
sivar <sivar@SIVAMAREEDU>
2024-02-15 03:03:22 +0530

    Add project files.

---
+++ BundleSolution/BundleElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BundleSolution
+{
+    public class BundleElement
+    {
+        public string Name { get; set; }
+        public int Units { get; set; }
+        public int Inventory { get; set; }
+
+        public BundleElement(string name, int unit = 0, int inventory = 0)
+        {
+            Name = name;
+            Units = unit;
+            Inventory = inventory;
+        }
+    }
+}
---
+++ BundleSolution/Program.cs
@@ -0,0 +1,56 @@
+﻿using BundleSolution;
+using System.Security.Cryptography;
+
+GeneralTree<BundleElement> bike = new GeneralTree<BundleElement>(new BundleElement("Bike"));
+GeneralTree<BundleElement> seat = new GeneralTree<BundleElement>(new BundleElement("Seat", 1, 50));
+GeneralTree<BundleElement> pedal = new GeneralTree<BundleElement>(new BundleElement("Pedal", 2, 60));
+GeneralTree<BundleElement> wheel = new GeneralTree<BundleElement>(new BundleElement("Wheel", 2));
+GeneralTree<BundleElement> frame = new GeneralTree<BundleElement>(new BundleElement("Frame", 1, 60));
+GeneralTree<BundleElement> tube = new GeneralTree<BundleElement>(new BundleElement("Tube", 1, 35));
+
+wheel.AddSubTree(frame);
+wheel.AddSubTree(tube);
+bike.AddSubTree(seat);
+bike.AddSubTree(pedal);
+bike.AddSubTree(wheel);
+
+var result = bike.Preorder();
+
+foreach (var r in result)
+{
+    Console.WriteLine($"Name - {r.Name}; Units - {r.Units}; Inventory - {r.Inventory};");
+}
+
+Console.WriteLine();
+Console.WriteLine($"Maximum number of bundles we can construct with Inventory: {ComputeMaxBundleCount(bike)}");
+
+int ComputeMaxBundleCount(GeneralTree<BundleElement> elements)
+{
+    if (elements.IsLeaf())
+        return 0;
+    var firstElement = ((GeneralTree<BundleElement>)elements.GetChildren(0)).GetRoot();
+    int min = firstElement.Inventory / firstElement.Units;
+    return ComputeMaxBundleCountHelper(elements, ref min);
+}
+
+int ComputeMaxBundleCountHelper(GeneralTree<BundleElement> elements, ref int min)
+{
+    for (int i = 0; i < elements.NumberSubTrees(); i++)
+    {
+        ComputeMaxBundleCountHelper(((GeneralTree<BundleElement>)elements.GetChildren(i)), ref min);
+    }
+
+    var root = elements.GetRoot();
+
+    if (elements.IsLeaf())
+    {
+        var partStock = root.Inventory / root.Units;
+        if (min > partStock)
+            min = partStock;
+    }
+    else if(root.Units != 0)
+    {
+        min = min / root.Units;
+    }
+    return min;
+}
