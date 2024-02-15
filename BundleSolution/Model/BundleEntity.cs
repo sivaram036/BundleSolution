ba837e9131cb908c732b3f28976f2a067befc09f
siva ramakrishna <sivaram.mareedu@hotmail.com>
2024-02-15 19:09:13 +0530

    cleanup the code

--- BundleSolution/Model/BundleEntity.cs
+++ BundleSolution/Model/BundleEntity.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace BundleSolution.Model
 {
-    /// <summary>
-    /// Genaric Bundle Class
-    /// </summary>
     public class BundleEntity
     {
         public int Id { get; set; }
